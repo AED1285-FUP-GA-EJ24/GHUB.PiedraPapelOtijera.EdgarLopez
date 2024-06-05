76e3595a9bf6c0d9889d4f8bc0f976155f2eeb7f
Edgar Jesus Lopez Perez <159453307+edgarlpz13@users.noreply.github.com>
2024-06-05 00:56:11 +0000

    trabajo

---
+++ piedraPapelTijera/Principal.java
@@ -0,0 +1,9 @@
+package piedraPapelTijera;
+
+public class Principal{
+    public static void main(String[] args) {
+        PiedraPapelTijera objPiedraPapelTijera = new PiedraPapelTijera();
+        objPiedraPapelTijera.Lanzar();
+        
+    }
+}
